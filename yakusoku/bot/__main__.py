9e434e08bad85d814958f976318031760131393e
Ricky8955555 <rkmiao@duck.com>
2023-07-09 23:58:51 +0800

    chore: configure pyright and flake8

--- yakusoku/bot/__main__.py
+++ yakusoku/bot/__main__.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from aiogram import Bot, Dispatcher  # type: ignore
+from aiogram import Bot, Dispatcher
 import aiogram
 
 from . import modules
@@ -10,10 +10,13 @@ from ..common.config import Config
 @dataclass
 class BotConfig(Config):
     token: str
+    skip_updates: bool = False
 
 
 config = BotConfig.load("bot/bot")
 bot = Bot(config.token)
 dp = Dispatcher(bot)
 modules.run(dp)
-aiogram.executor.start_polling(dp, skip_updates=True)
+aiogram.executor.start_polling(
+    dp, skip_updates=config.skip_updates
+)
