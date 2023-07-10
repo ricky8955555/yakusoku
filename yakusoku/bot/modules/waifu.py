760d4559e0abc382fb3fcc1d52a170592b304d7e
Ricky8955555 <rkmiao@duck.com>
2023-07-10 13:10:44 +0800

    feat: waifu

---
+++ yakusoku/bot/modules/waifu.py
@@ -0,0 +1,112 @@
+import contextlib
+import random
+from dataclasses import dataclass
+from datetime import datetime
+from tempfile import TemporaryFile
+
+from aiogram.dispatcher.filters import ChatTypeFilter
+from aiogram.types import Chat, ChatMember, ChatPhoto, ChatType, Message, User
+from sqlitedict import SqliteDict
+
+from .. import database
+from ..shared import members
+from . import dispatcher
+
+dp = dispatcher()
+DATABASE_NAME = "waifu"
+
+filtered = [
+    777000,
+    136817688,
+    609517172,
+    1031952739,
+    1087968824,
+    5304501737,
+    dp.bot.id,
+]
+
+
+@dataclass
+class MemberWaifuInfo:
+    last: datetime
+    waifu: int
+
+
+async def random_member(group: Chat, sender: User) -> ChatMember:
+    members_ = await members.get_members(group)
+    return random.choice(
+        [
+            member
+            for member in members_
+            if member.user.id not in filtered and member.user.id != sender.id
+        ]
+    )
+
+
+def get_db(group: Chat) -> SqliteDict:
+    return database.get(DATABASE_NAME, str(group.id))
+
+
+def get_waifu_info(group: Chat, sender: User) -> MemberWaifuInfo | None:
+    entry: tuple[int, int] | None = get_db(group).get(  # type: ignore
+        sender.id
+    )
+    return (
+        MemberWaifuInfo(
+            datetime.fromtimestamp(entry[0]), entry[1]  # type: ignore
+        )
+        if entry
+        else None
+    )
+
+
+def update_waifu_info(
+    group: Chat,
+    sender: User,
+    waifu: MemberWaifuInfo,
+) -> None:
+    db = get_db(group)
+    db[sender.id] = (int(waifu.last.timestamp()), waifu.waifu)
+
+
+async def fetch_waifu(
+    group: Chat,
+    sender: User,
+) -> tuple[bool, ChatMember]:
+    info = get_waifu_info(group, sender)
+    now = datetime.now()
+    if not info or info.last.date() < now.date():
+        member = await random_member(group, sender)
+        info = MemberWaifuInfo(now, member.user.id)
+        update_waifu_info(group, sender, info)
+        return True, member
+    else:
+        member = await group.get_member(info.waifu)
+        return False, member
+
+
+async def get_user_avatar(user: User) -> ChatPhoto:
+    return (await user.bot.get_chat(user.id)).photo
+
+
+@dp.message_handler(
+    ChatTypeFilter([ChatType.GROUP, ChatType.SUPERGROUP]),  # type: ignore
+    commands=["waifu"],
+)
+async def waifu(message: Message):
+    try:
+        updated, waifu = await fetch_waifu(message.chat, message.from_user)
+    except Exception as ex:
+        return await message.reply(f"找不到对象力(悲) www, 错误信息:\n{str(ex)}")
+
+    comment = (
+        "每天一老婆哦~ 你今天已经抽过老婆了喵w. " if not updated else ""
+    ) + f"你今天的老婆是 {waifu.user.get_mention(as_html=True)}"
+
+    with contextlib.suppress(Exception):
+        avatar_file = await get_user_avatar(waifu.user)
+        with TemporaryFile() as fp:
+            await avatar_file.download_small(fp)
+            return await message.reply_photo(fp, comment, parse_mode="HTML")
+
+    await message.reply(comment, parse_mode="HTML")
