0b329bc06ae3003ba469e079e333fa3207fd4fe5
Ricky8955555 <rkmiao@duck.com>
2023-07-10 13:01:50 +0800

    feat: start command

---
+++ yakusoku/bot/constants.py
@@ -0,0 +1,2 @@
+GITHUB_REPOSITORY_URL = "https://github.com/ricky8955555/yakusoku"
+GITLAB_REPOSITORY_URL = "https://gitlab.huajitech.net/Ricky8955555/yakusoku"
---
+++ yakusoku/bot/modules/start.py
@@ -0,0 +1,18 @@
+from aiogram.types import Message
+
+from .. import constants
+from . import dispatcher
+
+dp = dispatcher()
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: Message):
+    await message.reply(
+        "欢迎使用 yakusoku (約束, 约定)\n\n"
+        "yakusoku 是一个多功能机器人, 有什么想法的都可以提出 Issue, 也欢迎大家来提交 PR.\n\n"
+        f"Git 仓库: [GitLab]({constants.GITLAB_REPOSITORY_URL}) (仅有公开查看权限)  "
+        f"[GitHub]({constants.GITHUB_REPOSITORY_URL})",
+        parse_mode="Markdown",
+        disable_web_page_preview=True,
+    )
