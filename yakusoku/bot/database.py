6d9906c5cfb8f754e82aea50edf5d3bceb97ccc9
Ricky8955555 <rkmiao@duck.com>
2023-07-10 13:08:05 +0800

    fix: error on file or dir does not exists

--- yakusoku/bot/database.py
+++ yakusoku/bot/database.py
@@ -1,9 +1,12 @@
 import os
+from typing import Any
 
 from sqlitedict import SqliteDict
 
 DATA_PATH = os.path.abspath("./data/bot/")
 
+os.makedirs(DATA_PATH, exist_ok=True)
 
-def get(name: str, table: str) -> SqliteDict:
+
+def get(name: str, table: str) -> Any:
     return SqliteDict(os.path.join(DATA_PATH, name), table, autocommit=True)
--- yakusoku/common/config.py
+++ yakusoku/common/config.py
@@ -7,6 +7,8 @@ BASE_PATH = os.path.abspath("./config/")
 
 
 def load_yaml(path: str) -> Any:
+    if not os.path.exists(path):
+        return None
     with open(path) as fp:
         return yaml.safe_load(fp)
 
@@ -14,4 +16,6 @@ def load_yaml(path: str) -> Any:
 class Config:
     @classmethod
     def load(cls, name: str):
-        return cls(**load_yaml(os.path.join(BASE_PATH, name + ".yml")))
+        path = os.path.join(BASE_PATH, name + ".yml")
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        return cls(**(data if (data := load_yaml(path)) else {}))
