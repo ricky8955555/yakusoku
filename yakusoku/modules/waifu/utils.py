ceda97cff3944bc87afa54e42e7678ed5269f335
Ricky8955555 <rkmiao@duck.com>
2023-07-14 21:43:05 +0800

    fix: type hint

--- yakusoku/modules/waifu/utils.py
+++ yakusoku/modules/waifu/utils.py
@@ -10,7 +10,7 @@ def local_or_global(
     key: Callable[[WaifuLocalProperty | WaifuGlobalProperty], _T | None],
     chat: int,
     member: int,
-) -> _T:
-    local: _T | None = key(factory.get_waifu_local_property(chat, member))
-    global_: _T = key(factory.get_waifu_global_property(member))  # type: ignore
+) -> _T | None:
+    local = key(factory.get_waifu_local_property(chat, member))
+    global_ = key(factory.get_waifu_global_property(member))
     return global_ if local is None else local
