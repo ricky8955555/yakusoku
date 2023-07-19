991c2954806b6b5bdbd1c09ee48a6e70b8751bed
Ricky8955555 <rkmiao@duck.com>
2023-07-19 19:57:09 +0800

    fix: path for database storage

--- yakusoku/database.py
+++ yakusoku/database.py
@@ -6,8 +6,8 @@ from sqlitedict import SqliteDict
 from yakusoku.constants import DATA_PATH
 
 DATABASE_PATH = os.path.join(DATA_PATH, "database")
-os.makedirs(DATA_PATH, exist_ok=True)
+os.makedirs(DATABASE_PATH, exist_ok=True)
 
 
 def get(name: str, table: str) -> Any:
-    return SqliteDict(os.path.join(DATA_PATH, name + ".sqlite"), table, autocommit=True)
+    return SqliteDict(os.path.join(DATABASE_PATH, name + ".sqlite"), table, autocommit=True)
