bb50dbd996cabcd6eaaa01c2fea77c1aa2cae288
Ricky8955555 <rkmiao@duck.com>
2023-10-02 07:53:30 +0800

    feat: filter invalid waifus when filtering invalid members

--- scripts/filter_invalid_members.py
+++ scripts/filter_invalid_members.py
@@ -3,6 +3,8 @@ import asyncio
 from sqlalchemy.exc import NoResultFound
 
 from yakusoku.archive import group_manager, user_manager
+from yakusoku.modules.waifu.manager import WaifuManager
+from yakusoku.modules.waifu.registry import Registry as WaifuRegistry
 
 
 async def main():
@@ -12,6 +14,16 @@ async def main():
                 _ = await user_manager.get_user(member)
             except NoResultFound:
                 await group_manager.remove_member(group.id, member)
+
+        waifu_manager = WaifuManager()
+        waifu_registry = WaifuRegistry(waifu_manager)
+        for waifu in await waifu_manager.get_waifu_datas(group.id):
+            try:
+                _ = await user_manager.get_user(waifu.member)
+            except NoResultFound:
+                if waifu.get_partner():
+                    await waifu_registry.divorce(waifu.group, waifu.member)
+                await waifu_manager.remove_waifu(waifu.group, waifu.member)
 
 
 asyncio.run(main())
