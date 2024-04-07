21a40275e60559fcd6afd901f154c5138cf4ab0b
Ricky8955555 <rkmiao@duck.com>
2024-04-07 22:41:53 +0800

    fix(dot): positional parameter problem at inform patch

--- yakusoku/dot/inform/message.py
+++ yakusoku/dot/inform/message.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Any, cast
 
 from aiogram.types import ChatType, Message, ParseMode
@@ -8,86 +9,96 @@ from yakusoku.utils import chat
 
 @patch(Message)
 class PatchedMessage:
-    def _process(self: Any, text: str, kwargs: dict[str, Any]) -> str:
+    def _handle(self: Any, raw: Any, args: tuple[Any, ...], kwargs: dict[str, Any]) -> str:
         message = cast(Message, self)
         inform = kwargs.pop("inform", True)
-        if not inform or message.chat.type not in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            return text
-        parse_mode = kwargs.get("parse_mode") or message.bot.parse_mode
-        if not parse_mode:
-            kwargs["parse_mode"] = parse_mode = ParseMode.HTML
+        if (
+            not inform
+            or message.chat.type not in [ChatType.GROUP, ChatType.SUPERGROUP]
+            or message.from_user.id == message.bot.id
+        ):
+            return raw(*args, **kwargs)
+
+        sig = inspect.signature(raw)
+        params = list(sig.parameters.keys())
+
+        modified_args = list(args)
+
+        parse_mode_index = params.index("parse_mode")
+        text_index, text_param = next(
+            (index, param) for index, param in enumerate(params) if param in ["text", "caption"]
+        )
+
+        if not (parse_mode := kwargs.get("parse_mode")):
+            if len(args) > parse_mode_index:
+                modified_args[parse_mode_index] = parse_mode = (
+                    args[parse_mode_index] or ParseMode.HTML
+                )
+            else:
+                kwargs["parse_mode"] = parse_mode = ParseMode.HTML
+
         as_html = cast(bool, parse_mode == ParseMode.HTML)
         mention = chat.get_mention(message.sender_chat or message.from_user, as_html=as_html)
-        return f"{mention} {text}"
+
+        if (text := kwargs.get(text_param) or "") or len(args) <= text_index:
+            kwargs[text_param] = mention + text
+        else:
+            modified_args[text_index] = mention + args[text_index]
+
+        return raw(*args, **kwargs)
 
     @patched
-    def reply(self: Any, text: str, *args: Any, **kwargs: Any) -> Any:
-        return self.__old_reply(PatchedMessage._process(self, text, kwargs), *args, **kwargs)
+    def reply(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply, args, kwargs)
 
     @patched
-    def answer(self: Any, text: str, *args: Any, **kwargs: Any) -> Any:
-        return self.__old_answer(PatchedMessage._process(self, text, kwargs), *args, **kwargs)
+    def answer(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer, args, kwargs)
 
     @patched
-    def reply_photo(self: Any, photo: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_reply_photo(
-            photo, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def reply_photo(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply_photo, args, kwargs)
 
     @patched
-    def answer_photo(self: Any, photo: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_answer_photo(
-            photo, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def answer_photo(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer_photo, args, kwargs)
 
     @patched
-    def reply_audio(self: Any, video: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_reply_audio(
-            video, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def reply_audio(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply_audio, args, kwargs)
 
     @patched
-    def answer_audio(self: Any, video: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_answer_audio(
-            video, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def answer_audio(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer_audio, args, kwargs)
 
     @patched
-    def reply_document(
-        self: Any, document: Any, caption: str = "", *args: Any, **kwargs: Any
-    ) -> Any:
-        return self.__old_reply_document(
-            document, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def reply_document(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply_document, args, kwargs)
 
     @patched
-    def answer_document(
-        self: Any, document: Any, caption: str = "", *args: Any, **kwargs: Any
-    ) -> Any:
-        return self.__old_answer_document(
-            document, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def answer_document(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer_document, args, kwargs)
 
     @patched
-    def reply_video(self: Any, video: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_reply_video(
-            video, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def reply_video(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply_video, args, kwargs)
 
     @patched
-    def answer_video(self: Any, video: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_answer_video(
-            video, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def answer_video(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer_video, args, kwargs)
 
     @patched
-    def reply_voice(self: Any, voice: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_reply_voice(
-            voice, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def reply_voice(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply_voice, args, kwargs)
 
     @patched
-    def answer_voice(self: Any, voice: Any, caption: str = "", *args: Any, **kwargs: Any) -> Any:
-        return self.__old_answer_voice(
-            voice, PatchedMessage._process(self, caption, kwargs), *args, **kwargs
-        )
+    def answer_voice(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer_voice, args, kwargs)
+
+    @patched
+    def reply_animation(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_reply_animation, args, kwargs)
+
+    @patched
+    def answer_animation(self: Any, *args: Any, **kwargs: Any) -> Any:
+        return PatchedMessage._handle(self, self.__old_answer_animation, args, kwargs)
