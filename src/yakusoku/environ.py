16bd1d49e3c89ca685008f43578dd16c84c3b73e
Ricky8955555 <rkmiao@duck.com>
2024-07-07 21:17:18 +0800

    feat(environ): create dir automatically

--- src/yakusoku/environ.py
+++ src/yakusoku/environ.py
@@ -4,5 +4,9 @@ from pathlib import Path
 working_dir = Path(os.getcwd())
 
 module_path = Path(__file__).parent / "modules"
+
 data_path = working_dir / "data"
+data_path.mkdir(exist_ok=True)
+
 config_path = working_dir / "config"
+config_path.mkdir(exist_ok=True)
