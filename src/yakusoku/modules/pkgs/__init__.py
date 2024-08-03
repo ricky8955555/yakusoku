c2b86865e7e54b7dfd3145f0567aed24d3682468
Ricky8955555 <rkmiao@duck.com>
2024-08-03 14:17:32 +0800

    docs(pkgs): add 'pkgst' description

--- src/yakusoku/modules/pkgs/__init__.py
+++ src/yakusoku/modules/pkgs/__init__.py
@@ -1,5 +1,7 @@
 from yakusoku.module import ModuleConfig
 
 __module_config__ = ModuleConfig(
-    name="pkgs", description="Linux 软件包信息", commands={"pkgs": "获取 Linux 软件包信息"}
+    name="pkgs",
+    description="Linux 软件包信息",
+    commands={"pkgs": "获取 Linux 软件包信息", "pkgst": "获取 pkgs 状态"},
 )
